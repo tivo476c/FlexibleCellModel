55b3099b36930c8dc4b21536630bdf57486f5192
tivo476c <tivo476c@tu-dresden.de>
2025-08-11 12:11:18 +0200

    start first soft df sim

--- code/simulationFunctionalities.jl
+++ code/simulationFunctionalities.jl
@@ -364,7 +364,7 @@ function giveClosestTimeIdx(sol, wantTime)
         end
     end
 
-    println("warning in giveClosestTime: wantTime", wantTime, " is larger than latest solution time")
+    # println("warning in giveClosestTime: wantTime $wantTime is larger than latest solution time $(sol.t[end])")
     return length(sol.t)
 end
 
@@ -501,30 +501,30 @@ function runSimulation_locations()
     end
 
     # 1st save one simulation as gif 
-    println("save one sim as gif")
+    # println("save one sim as gif")
 
-    if N == 0
-        u0 = InitializePointParticles(radius)
-    else
-        u0 = initializeCells(radius)
-    end
+    # if N == 0
+    #     u0 = InitializePointParticles(radius)
+    # else
+    #     u0 = initializeCells(radius)
+    # end
 
-    cellProblem = SDEProblem(energies!, brownian_DF!, u0, timeInterval, p, noise_rate_prototype=zeros(2 * M * N, 2 * M))
-    @time sol = solve(cellProblem,
-        EM(),
-        # callback=CallBack_reflectiveBC_cellOverlap,
-        dt=timeStepSize,
-    )
-    extractedSol = extractSolution(sol)
-    createSimGif(gifPath, extractedSol)
+    # cellProblem = SDEProblem(energies!, brownian_DF!, u0, timeInterval, p, noise_rate_prototype=zeros(2 * M * N, 2 * M))
+    # @time sol = solve(cellProblem,
+    #     EM(),
+    #     # callback=CallBack_reflectiveBC_cellOverlap,
+    #     dt=timeStepSize,
+    # )
+    # extractedSol = extractSolution(sol)
+    # createSimGif(gifPath, extractedSol)
 
     ### 2nd: CREATE ALL POINT LOCATIONS FOR ALL SIMULATIONS 
-    # results = pmap(do1SimulationRun, 1:NumberOfSimulations)
+    results = pmap(do1SimulationRun, 1:NumberOfSimulations)
 
     ### 3rd: CREATE THE HEATMAP FROM ALL SIMULATION DATA 
-    # heatmatrices = makeMatrices()
+    heatmatrices = makeMatrices()
     # smoothenMatrix!(heatmatrices, 30)
-    # createHeatmaps(heatmatrices)
+    createHeatmaps(heatmatrices)
 
 end
 
