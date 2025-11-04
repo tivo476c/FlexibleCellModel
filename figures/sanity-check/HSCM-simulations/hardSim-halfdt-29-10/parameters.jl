4664ca6ce1c74945f29bdf16f90c1f578f415fdd
tivo476c <tivo476c@tu-dresden.de>
2025-11-04 21:46:01 +0100

    added new hard sim with half dt

---
+++ figures/sanity-check/HSCM-simulations/hardSim-halfdt-29-10/parameters.jl
@@ -0,0 +1,48 @@
+## General setup:
+
+domainL = 0.5                               # 0.5 / 40.0
+domain = (-domainL, domainL)                # domain where cells can move: [-5.0, 5.0]^2 
+NumberOfCellWallPoints = 3                  # number of wall points per cell [OLD NAME: "N"]
+N = NumberOfCellWallPoints
+NumberOfCells = 400                        # number of cells [OLD NAME: "M"] TODO: change to 400
+M = NumberOfCells
+D = 1                                       # diffusitivity constant 
+radius = 0.005                              # cell radius 
+
+## Force scalings: 
+hardness = 1                                       # tells how hard the cells are hardness =1 for hard cells, hardness =0 for soft cells, or something in between 
+
+# areaForceFactor = (1 - hardness) * 1e6 + hardness * 1e10
+# edgeForceFactor = (1 - hardness) * 1e1 + hardness * 1e5
+# interiorAngleForceFactor = (1 - hardness) * 1e0 + hardness * 1e2
+# overlapForceFactor = 2e4
+areaForceFactor = 0
+edgeForceFactor = 0
+interiorAngleForceFactor = 0
+overlapForceFactor = 0
+overlapForceTypes = ["bachelorThesis", "radiusBilliard", "combination"]
+overlapForceType = overlapForceTypes[2]
+
+forceScalings = [areaForceFactor, edgeForceFactor, interiorAngleForceFactor, overlapForceFactor]
+
+## Simulation time parameters: 
+timeStepSize = 0.5*10^(-5)
+T = 0.05
+timeInterval = (0.0, T)
+
+NumberOfSimulations = 6*10^3
+NumberOfSampleTimes = 6          # must be 2 at least
+sampleTimes = [T * k / (NumberOfSampleTimes - 1) for k = 0:NumberOfSampleTimes-1]
+# sampleTimesRange = 0:T/(NumberOfSampleTimes-1):T
+
+## Simulation name 
+# simulationName = "drift-$(floor(Int64, log10(areaForceFactor)))-$(floor(Int64,log10(edgeForceFactor)))-$(floor(Int64, log10(interiorAngleForceFactor)))-$(floor(Int64,log10(overlapForceFactor)))"
+simulationName = "hardSim-halfdt-29-10" 
+
+
+
+## Space Discretisation for heatmap 
+NumberOfHeatGridPoints = 30
+HeatStepSize = 2 * domainL / NumberOfHeatGridPoints
+# [-L, -L + HeatStepSize, -L + 2*HeatStepSize, ..., -L + NumberOfHeatGridPoints*HeatStepSize]
+HeatGrid = [-domainL + k * HeatStepSize for k in 0:NumberOfHeatGridPoints]
